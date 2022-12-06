8f932c43d372ce70b460af38dcbcd2f6b0a1c0fa
Gautam <kumawatgautam305@gmail.com>
2022-12-06 16:24:42 +0530

    all ok

--- gks.js
+++ gks.js
@@ -40,7 +40,12 @@ app.post("/",function(req,res)
     });
 });
 
-app.listen(3000,function()
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port,function()
 {
     console.log("server started...");
 });
